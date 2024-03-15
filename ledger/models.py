bfc6f41736ac31094e75385642973db7b63bb7cc
Orangguu <jklwarain@gmail.com>
2024-03-15 15:46:53 +0800

    modified models.py, added profile and editten the recipe model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
 
 
 class Ingredient(models.Model):
@@ -13,7 +20,11 @@ class Ingredient(models.Model):
 
 
 class Recipe(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
     name = models.CharField(max_length=50)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
